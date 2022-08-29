54e8ee8a8fea70702d7d109ddb0ba6e305961a5d
ba63 <ba63@nyu.edu>
2022-08-29 13:57:20 -0400

    Inference bash script

---
+++ timeline-extraction/bert-lstm/inference.sh
@@ -0,0 +1,13 @@
+python -W ignore bert_lstm.py \
+    --bert_model bert-base-uncased \
+    --train_file /home/balhafni/timeline-summarization/new_data/train.json \
+    --dev_file /home/balhafni/timeline-summarization/new_data/dev.json \
+    --num_epochs 30 \
+    --learning_rate 5e-5 \
+    --hidd_size 128 \
+    --num_layers 1 \
+    --batch_size 16 \
+    --dropout 0.1 \
+    --do_predict \
+    --checkpoint checkpoints_new/checkpoint-best \
+    --seed 42
