e39ad5dc73ae74c20cf3324e49216777c404aac6
balhafni <ba63@nyu.edu>
2022-10-21 18:17:31 +0400

    Inference bash script

--- timeline-extraction/bert/inference.sh
+++ timeline-extraction/bert/inference.sh
@@ -1,6 +1,16 @@
-# models/bert-relevant-only/checkpoint-4500
+#!/bin/bash
+#SBATCH -p nvidia
+# use gpus
+#SBATCH --gres=gpu:1
+# memory
+#SBATCH --mem=120000
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
 
 python inference.py \
-    --eval_data_file /home/balhafni/timeline-summarization/data/test.json \
-    --model_path models_new/bert-relevant-only/checkpoint-4500-best \
+    --eval_data_file /scratch/ba63/CrisisLTLSum/data/test.json \
+    --model_path  /scratch/ba63/CrisisLTLSum/timeline-extraction/bert-relevant-only/checkpoint-2000-best \
     --max_seq_length 512
