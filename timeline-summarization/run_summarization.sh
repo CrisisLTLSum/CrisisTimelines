cd0293db2a96c307ea71e5909347772c44c9bfcc
balhafni <ba63@nyu.edu>
2022-10-21 18:21:36 +0400

    Summarization fine-tuning bash script

--- timeline-summarization/run_summarization.sh
+++ timeline-summarization/run_summarization.sh
@@ -1,44 +1,27 @@
+#!/bin/bash
+#SBATCH -p nvidia
+# use gpus
+#SBATCH --gres=gpu:1
+# memory
+#SBATCH --mem=120000
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
 # facebook/bart-base
 # sshleifer/distilbart-cnn-12-6
 
-# python run_summarization.py \
-#      --model_name_or_path facebook/bart-base \
-#      --do_train \
-#      --train_file  /home/balhafni/timeline-summarization/new_data/train.sum.json \
-#      --num_train_epochs 10 \
-#      --text_column timeline \
-#      --summary_column summary \
-#      --output_dir models_new/bart \
-#      --per_device_train_batch_size 8 \
-#      --max_target_length 512 \
-#      --seed 42 \
-#      --overwrite_output_dir \
-#      --overwrite_cache
-
-
-# python run_summarization.py \
-#     --model_name_or_path sshleifer/distilbart-cnn-12-6 \
-#     --do_train \
-#     --train_file  /home/balhafni/timeline-summarization/new_data/train.sum.json \
-#     --num_train_epochs 10 \
-#     --text_column timeline \
-#     --summary_column summary \
-#     --output_dir models_new/distill-bart \
-#     --per_device_train_batch_size 4 \
-#     --max_target_length 512 \
-#     --seed 42 \
-#     --overwrite_output_dir \
-#    --overwrite_cache 
-
 python run_summarization.py \
-     --model_name_or_path facebook/bart-base \
+     --model_name_or_path /scratch/ba63/BERT_models/distilbart-cnn-12-6 \
      --do_train \
-     --train_file  /home/balhafni/timeline-summarization/data/extractive_models_outputs/sent_level_combs/sent_level_combs.oracle.train.json \
+     --train_file  /scratch/ba63/CrisisLTLSum/data/train.sum.json \
      --num_train_epochs 10 \
      --text_column timeline \
      --summary_column summary \
-     --output_dir extractive_with_bart_models/sent_level_combs \
-     --per_device_train_batch_size 8 \
+     --output_dir /scratch/ba63/CrisisLTLSum/timeline-summarization/distilbart \
+     --per_device_train_batch_size 16 \
      --max_target_length 512 \
      --seed 42 \
      --overwrite_output_dir \
