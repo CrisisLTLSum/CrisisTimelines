ae017d2b6bea509ae14c4e728954532f1f95b86f
balhafni <ba63@nyu.edu>
2022-10-21 18:17:41 +0400

    Script to run inference over all checkpoints

--- timeline-extraction/bert/inference_checkpoints.sh
+++ timeline-extraction/bert/inference_checkpoints.sh
@@ -1,8 +1,20 @@
-for checkpoint in models_new/bert-relevant-only models_new/bert-relevant-only/checkpoint*
-do
-echo "Inference using $checkpoint..."
-python inference.py \
-    --eval_data_file /home/balhafni/timeline-summarization/new_data/dev.json \
-    --model_path  $checkpoint \
-    --max_seq_length 512
-done
+#!/bin/bash
+#SBATCH -p nvidia
+# use gpus
+#SBATCH --gres=gpu:1
+# memory
+#SBATCH --mem=120000
+# Walltime format hh:mm:ss
+#SBATCH --time=11:30:00
+# Output and error files
+#SBATCH -o job.%J.out
+#SBATCH -e job.%J.err
+
+for checkpoint in /scratch/ba63/CrisisLTLSum/timeline-extraction/bert-relevant-only/checkpoint*
+    do
+        echo "Inference using $checkpoint..."
+        python inference.py \
+            --eval_data_file /scratch/ba63/CrisisLTLSum/data/dev.json \
+            --model_path  $checkpoint \
+            --max_seq_length 512
+    done
